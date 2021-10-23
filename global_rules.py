1e574d019dd8b1f742cdc720d97c3362e3f47405
Zachary Preator <zachary.preator@gmail.com>
2021-10-23 12:50:50 -0600

    Added line for testing

--- global_rules.py
+++ global_rules.py
@@ -11,10 +11,15 @@ def run(data, bot_info, send):
         send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
         return True
 
+    if message == '.details':
+        send(str(data), bot_info[0])
+        return True
+
     # Checks all the possible triggers from responses.txt
-    result = check_responses('responses.txt', message)
-    if result is not None:
-        send(result, bot_info[0])
+    # result = check_responses('responses.txt', message)
+    # if result is not None:
+    #     send(result, bot_info[0])
+        # return True
     return True
 
 
