16d6a86ef55ec368f839cac9c840e5cc75b088e6
Zachary Preator <zachary.preator@gmail.com>
2021-10-23 22:50:11 -0600

    Add better help menu Add stub for markov response

--- global_rules.py
+++ global_rules.py
@@ -4,6 +4,7 @@ def run(data, bot_info, send):
     message = data['text']
 
     sender = data['sender_id']
+    sender_name = data['name']
 
     # Check if bot is sender, do exit if so (to avoid feedback loops)
     if sender == '861991':
@@ -11,7 +12,11 @@ def run(data, bot_info, send):
         return True
 
     if message == '.help':
-        help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
+        help_message = "Help:"\
+                       "\n.help   -->  This screen"\
+                       "\n.test   -->  For debugging"\
+                       "\n.joke   -->  Gets a Dad joke"\
+                       "\n.swerve -->  Compiles a response based on chat history"
         send(help_message, bot_info[0])
         return True
 
@@ -26,6 +31,10 @@ def run(data, bot_info, send):
     if message.lower() == '.joke' or message.lower() == '.jokes':
         msg = get_random_joke()
         send(msg, bot_info[0])
+
+    if message.lower() == '.swerve':
+        # msg = get_response()
+        send('Sorry, this is still in development', bot_info[0])
 
     if '.get_bot_id' in message:
         send('bot_id: {0}'.format(str(sender)), bot_info[0])
@@ -51,6 +60,14 @@ def check_responses(path, msg):
 
 def get_random_joke():
     with open('jokes.txt', 'r') as file:
+        lines = file.readlines()
+        index = random.randint(0, len(lines) - 1)
+        selected_line = lines[index]
+        return selected_line
+
+
+def get_response():
+    with open('markov_responses.txt', 'r') as file:
         lines = file.readlines()
         index = random.randint(0, len(lines) - 1)
         selected_line = lines[index]
